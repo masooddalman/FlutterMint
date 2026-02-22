5ece9480ca0cea5772514808cd78856102c7b352
masooddalman <m.dalman68@gmail.com>
2026-02-22 09:40:17 +0300

    feat: add CI/CD module with GitHub Actions workflow
    Generate .github/workflows/ci.yml with Flutter setup, analyze, test,
    and debug APK build steps. Enables coverage flag when testing module
    is also selected.

---
+++ lib/src/modules/cicd_module.dart
@@ -0,0 +1,47 @@
+import 'package:flutterforge/src/config/project_config.dart';
+import 'package:flutterforge/src/modules/module.dart';
+import 'package:flutterforge/src/templates/cicd/github_actions_template.dart';
+
+class CicdModule extends Module {
+  @override
+  String get id => 'cicd';
+
+  @override
+  String get displayName => 'CI/CD (GitHub Actions)';
+
+  @override
+  bool get isDefault => false;
+
+  @override
+  List<String> get dependsOn => [];
+
+  @override
+  Map<String, String> get dependencies => {};
+
+  @override
+  Map<String, String> get devDependencies => {};
+
+  @override
+  Map<String, String> generateFiles(ProjectConfig config) => {
+        '.github/workflows/ci.yml':
+            GithubActionsTemplate.generate(config),
+      };
+
+  @override
+  List<String> mainImports(ProjectConfig config) => [];
+
+  @override
+  List<String> mainSetupLines(ProjectConfig config) => [];
+
+  @override
+  List<String> locatorImports(ProjectConfig config) => [];
+
+  @override
+  List<String> locatorRegistrations(ProjectConfig config) => [];
+
+  @override
+  List<String> providerDeclarations(ProjectConfig config) => [];
+
+  @override
+  List<String> appImports(ProjectConfig config) => [];
+}
---
+++ lib/src/templates/cicd/github_actions_template.dart
@@ -0,0 +1,48 @@
+import 'package:flutterforge/src/config/project_config.dart';
+
+class GithubActionsTemplate {
+  GithubActionsTemplate._();
+
+  static String generate(ProjectConfig config) {
+    final hasTests = config.hasModule('testing');
+    final testStep = hasTests
+        ? '''
+      - name: Run tests
+        run: flutter test --coverage
+'''
+        : '''
+      - name: Run tests
+        run: flutter test
+''';
+
+    return '''name: CI
+
+on:
+  push:
+    branches: [main, develop]
+  pull_request:
+    branches: [main, develop]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v4
+
+      - uses: subosito/flutter-action@v2
+        with:
+          channel: stable
+
+      - name: Install dependencies
+        run: flutter pub get
+
+      - name: Analyze code
+        run: flutter analyze
+
+$testStep
+      - name: Build APK
+        run: flutter build apk --debug
+''';
+  }
+}
