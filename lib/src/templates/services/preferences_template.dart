6a4dcd0704f17aa9c5bb73a07d83da18e427aabe
masooddalman <m.dalman68@gmail.com>
2026-03-05 21:41:02 +0300

    feat: add PreferencesService and Riverpod providers template for SharedPreferences

---
+++ lib/src/templates/services/preferences_template.dart
@@ -0,0 +1,54 @@
+import 'package:fluttermint/src/config/project_config.dart';
+
+class PreferencesTemplate {
+  PreferencesTemplate._();
+
+  static String generatePreferencesService(ProjectConfig config) {
+    return '''import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferencesService {
+  PreferencesService(this._prefs);
+
+  final SharedPreferences _prefs;
+
+  String? getString(String key) => _prefs.getString(key);
+  Future<bool> setString(String key, String value) =>
+      _prefs.setString(key, value);
+
+  int? getInt(String key) => _prefs.getInt(key);
+  Future<bool> setInt(String key, int value) => _prefs.setInt(key, value);
+
+  double? getDouble(String key) => _prefs.getDouble(key);
+  Future<bool> setDouble(String key, double value) =>
+      _prefs.setDouble(key, value);
+
+  bool? getBool(String key) => _prefs.getBool(key);
+  Future<bool> setBool(String key, bool value) => _prefs.setBool(key, value);
+
+  List<String>? getStringList(String key) => _prefs.getStringList(key);
+  Future<bool> setStringList(String key, List<String> value) =>
+      _prefs.setStringList(key, value);
+
+  Future<bool> remove(String key) => _prefs.remove(key);
+
+  Future<bool> clear() => _prefs.clear();
+}
+''';
+  }
+
+  static String generatePreferencesProviders(ProjectConfig config) {
+    return '''import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:${config.appNameSnakeCase}/core/preferences/preferences_service.dart';
+
+final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError('Must be overridden in ProviderScope');
+});
+
+final preferencesServiceProvider = Provider<PreferencesService>((ref) {
+  return PreferencesService(ref.watch(sharedPreferencesProvider));
+});
+''';
+  }
+}
